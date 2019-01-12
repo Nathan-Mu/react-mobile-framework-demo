9ae8a163b2655a4e9b495a1a385f590f334bb826
Nathan-Mu <37648667+Nathan-Mu@users.noreply.github.com>
2019-01-13 00:51:30 +1100

    create fixed topbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Menu} from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import Div100vh from 'react-div-100vh';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div style={{position: 'fixed', top: 0, left: 0, right: 0, zIndex: 1}}>
+                    <TopBar/>
+                </div>
+
+                <div style={{position: 'relative', left: 0, top: 0, right: 0, backgroundColor: 'red'}}>
+                    <SideBar></SideBar>
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+class TopBar extends Component {
+    render() {
+        return (
+            <Menu>
+                <Menu.Item
+                    name='editorials'
+                    onClick={this.handleItemClick}
+                >
+                    open
+                </Menu.Item>
+            </Menu>
+        )
+    }
+}
+
+class SideBar extends Component {
+    render() {
+        return (
+            <Div100vh>
+            </Div100vh>
+        )
+    }
 }
 
 export default App;

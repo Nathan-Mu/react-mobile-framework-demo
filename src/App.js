e50f97d3102048c169933955506b2b818cb6190b
nathan <zhaody.mu@gmail.com>
2019-01-15 00:44:45 +1100

    modify app.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,51 @@ import 'semantic-ui-css/semantic.min.css';
 import Div100vh from 'react-div-100vh';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sidebarVisible: 'hidden'
+        };
+        this.handleMenuToggle = this.handleMenuToggle.bind(this);
+        this.handleMenuClose = this.handleMenuClose.bind(this);
+    }
+
+    handleMenuToggle() {
+        let {sidebarVisible} = this.state;
+        if (sidebarVisible === 'hidden') {
+            sidebarVisible = 'visible';
+        } else {
+            sidebarVisible = 'hidden';
+        }
+        this.setState({sidebarVisible});
+    }
+
+    handleMenuClose() {
+        this.setState({
+            sidebarVisible: 'hidden'
+        })
+    }
+
     render() {
         return (
             <div>
                 <div style={{position: 'fixed', top: 0, left: 0, right: 0, zIndex: 1}}>
-                    <TopBar/>
+                    <TopBar onMenuClick={this.handleMenuToggle}/>
                 </div>
 
-                <div style={{position: 'relative', left: 0, top: 0, right: 0, backgroundColor: 'red'}}>
-                    <SideBar></SideBar>
+                <div style={{
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    right: 0,
+                    visibility: this.state.sidebarVisible,
+                    zIndex: 2,
+                    webkitTransition: 'width 2s'
+                }}>
+                    <SideBar onMenuClose={this.handleMenuClose}></SideBar>
                 </div>
 
-                <div>
-
-                </div>
+                <Body/>
             </div>
         );
     }
@@ -31,7 +62,7 @@ class TopBar extends Component {
             <Menu>
                 <Menu.Item
                     name='editorials'
-                    onClick={this.handleItemClick}
+                    onClick={this.props.onMenuClick}
                 >
                     open
                 </Menu.Item>
@@ -43,7 +74,47 @@ class TopBar extends Component {
 class SideBar extends Component {
     render() {
         return (
+            <div>
+                <div style={{float: 'left', transition: 'left linear 1s', left: 0}}>
+                    <Div100vh style={{height: '100rvh', width: '60vw', backgroundColor: 'blue'}}>
+                    </Div100vh>
+                </div>
+                <div style={{float: 'left'}} onClick={this.props.onMenuClose}>
+                    <Div100vh style={{height: '100rvh', width: '40vw', backgroundColor: 'green'}}>
+                    </Div100vh>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Body extends Component {
+    render() {
+        return (
             <Div100vh>
+                <div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'blue', height: '80px'}}></div>
+                    <div style={{backgroundColor: 'red', height: '80px'}}></div>
+                </div>
             </Div100vh>
         )
     }
